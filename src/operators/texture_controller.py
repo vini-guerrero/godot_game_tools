60657040ae3b5de2f1e3ba063fbcce220e3bca0d
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2019-12-06 20:55:11 -0300

    Texture Baking Feature Updated

--- src/operators/texture_controller.py
+++ src/operators/texture_controller.py
@@ -4,16 +4,25 @@ from bpy.types import (Operator)
 
 class BAKE_TEXTURE_OT(bpy.types.Operator):
     bl_idname = "wm.bake_texture"
-    bl_label = "Bake Texture"
+    bl_label = "Bake Textures"
     bl_description = "Bakes Selected Texture"
 
     def execute(self, context):
-        # Validate Mesh With Material Is Selected
-        bpy.context.scene.render.engine = 'CYCLES'
+        scene = context.scene
+        tool = scene.godot_game_tools
+        textureSize = tool.bake_texture_size
         activeObj = bpy.context.view_layer.objects.active
-        bake_type = context.scene.cycles.bake_type
-        bpy.ops.object.bake('INVOKE_DEFAULT',type = bake_type)
-        self.report({'INFO'}, 'Bake Process Started')
+        # Validate Mesh With Material Is Selected
+        if activeObj is not None:
+            if activeObj.type == "MESH":
+                bpy.context.scene.render.engine = 'CYCLES'
+                bake_type = context.scene.cycles.bake_type
+                bpy.ops.object.bake('INVOKE_DEFAULT', save_mode='EXTERNAL', type=bake_type, width=textureSize, height=textureSize, filepath="//", margin=16)
+                self.report({'INFO'}, 'Bake Process Started')
+            else:
+                self.report({'INFO'}, 'Please select a valid mesh to bake the texture')
+        else:
+                self.report({'INFO'}, 'Please select a valid mesh to bake the texture')
         return {'FINISHED'}
 
 class SAVE_BAKE_TEXTURES_OT(Operator):
